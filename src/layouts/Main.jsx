f58f985089fa038d85ab45f166a0868444b1977b
nahid-dev <nahidv232@gmail.com>
2023-08-04 12:36:01 +0600

    primary style done

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,10 +4,12 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
-    <>
-      <Menu></Menu>
-      <Outlet></Outlet>
-    </>
+    <div className="bg-emerald-400 text-white h-screen flex justify-center items-center">
+      <div className="border">
+        <Menu></Menu>
+        <Outlet></Outlet>
+      </div>
+    </div>
   );
 };
 
