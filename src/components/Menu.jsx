9f49163fb7f1a3608531694d7b44c51bb86bb4ca
nahid-dev <nahidv232@gmail.com>
2023-08-04 17:03:02 +0600

    task form done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { HiPlusCircle } from "react-icons/hi";
+import { CgNotes } from "react-icons/cg";
 
 const Menu = () => {
   return (
-    <div>
-      <NavLink to="/">Tasks List</NavLink>
-      <NavLink to="/addATask">Add A Task</NavLink>
+    <div className="md:w-full border flex justify-between p-1">
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "default")}
+        to="/"
+      >
+        <span className="mr-2">Tasks List</span>{" "}
+        <span>
+          <CgNotes></CgNotes>
+        </span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "default")}
+        to="/addATask"
+      >
+        <span className="mr-2">Add New Task</span>{" "}
+        <span>
+          <HiPlusCircle></HiPlusCircle>
+        </span>
+      </NavLink>
     </div>
   );
 };
--- src/pages/AddATask.jsx
+++ src/pages/AddATask.jsx
@@ -1,9 +1,72 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { HiPlusCircle } from "react-icons/hi";
 
 const AddATask = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h3 className="text-4xl">Add A task</h3>
+    <div className="m-1">
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* title */}
+          <div className="flex w-full  flex-col my-5">
+            <label className="text-xl ml-3 font-semibold mb-3">Title</label>
+            <input
+              className="text-gray-600 p-3 text-xl bg-emerald-50 border-2 rounded-sm hover:border-emerald-800 transition-all duration-300"
+              type="text"
+              placeholder="Enter title"
+              {...register("title", { required: true })}
+            />
+            {errors.title && (
+              <span className="text-red-600">Title is required</span>
+            )}
+          </div>
+          {/* Description */}
+          <div className="flex w-full  flex-col my-3">
+            <label className="text-xl ml-3 font-semibold mb-3">
+              Description
+            </label>
+            <input
+              className="text-gray-600 p-3 text-xl bg-emerald-50 border-2 rounded-sm hover:border-emerald-800 transition-all duration-300"
+              type="text"
+              placeholder="Enter description"
+              {...register("description", { required: true })}
+            />
+            {errors.description && (
+              <span className="text-red-600">Description is required</span>
+            )}
+          </div>
+          {/* Status */}
+          <div className="flex w-full  flex-col my-3">
+            <label className="text-xl ml-3 font-semibold mb-3">Status</label>
+            <input
+              className="text-gray-600 p-3 text-xl bg-emerald-50 border-2 rounded-sm hover:border-emerald-800 transition-all duration-300"
+              type="text"
+              placeholder="Working"
+              readOnly
+            />
+          </div>
+          {/* Submit */}
+          <button
+            type="submit"
+            className="flex text-xl font-semibold hover:text-white transition-all duration-300 items-center justify-center hover:bg-emerald-500 border border-emerald-500 w-full md:py-3"
+          >
+            <span>Add</span>{" "}
+            <span>
+              <HiPlusCircle size={32}></HiPlusCircle>
+            </span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
